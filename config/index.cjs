c19969f46663c4c47f0d5fbaed4a416cbd3a28da
SPYCP <59533702+SPYCP@users.noreply.github.com>
2022-12-25 13:47:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -37,13 +37,13 @@ const USER_CONFIG = {
           type: '生日', name: '鹏鹏', year: '1998', date: '03-22',
         },
         {
-          type: '节日', name: '相恋纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相恋纪念日', year: '2022', date: '12-21',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-12-21' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
